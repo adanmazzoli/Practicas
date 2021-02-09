d0b3c40f9a7cfb10c41c2a555ac2eee810901adf
adanmazzoli <adanmazzoli@users.noreply.github.com>
2021-02-08 23:11:19 -0500

    Initial commit

---
+++ Practica 1/Ejercicio1.cs/Ejercicio1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejercicio1.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string name = "Adan Rainieri Mazzoli Batista";
+            Console.WriteLine ("Bienenido " +name);  
+        }
+    }
+}
